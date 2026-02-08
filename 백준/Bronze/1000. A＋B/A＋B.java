e79f76659fcbde3d064a0407e5e1fd6a161709e3
1jinie <wkddnjswlsn@naver.com>
2026-02-08 14:14:20 +0900

    Improve comments for better code understanding
    Added detailed comments to explain variable types and functionality.

--- 백준/Bronze/1000. A＋B/A＋B.java
+++ 백준/Bronze/1000. A＋B/A＋B.java
@@ -10,12 +10,13 @@ public class Main{
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));          // BufferedReader type variable br with InputStreamReader(String type)
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));        // BufferedWriter type variable bw with InputStreamWriter
         StringTokenizer st = new StringTokenizer(br.readLine());                           // StringTokenizer type variable st with readLine() : read line by line
-        int a = Integer.parseInt(st.nextToken());
+        int a = Integer.parseInt(st.nextToken());                                          // Convert Token to integer type
         int b = Integer.parseInt(st.nextToken());
-        bw.write(String.valueOf(a + b));
-        bw.flush();
-        bw.close();
+        bw.write(String.valueOf(a + b));                                                   // write the value of (a + b) on buffer 
+        bw.flush();                                                                        // force print all letters on buffer
+        bw.close();                                                                        // close stream
         br.close();
     }
 
 }
+
