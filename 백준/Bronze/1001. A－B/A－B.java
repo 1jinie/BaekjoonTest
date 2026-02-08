4b5abb66a7da820bce355eb8c93b598af3f02d0f
1jinie <wkddnjswlsn@naver.com>
2026-02-08 14:39:28 +0900

    Add comments for better understanding
    Added comments to clarify code functionality.

--- 백준/Bronze/1001. A－B/A－B.java
+++ 백준/Bronze/1001. A－B/A－B.java
@@ -1,10 +1,11 @@
-import java.util.Scanner;
+import java.util.Scanner;                            // import Scanner
 
 public class Main{
     public static void main(String[] args){
-        Scanner sc = new Scanner(System.in);
+        Scanner sc = new Scanner(System.in);         // declare and instantiate Scanner type sc
         int a = sc.nextInt();
         int b = sc.nextInt();
         System.out.println(a - b);
     }
+
 }
