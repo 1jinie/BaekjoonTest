f0ae6abaa40371b486ca7bde9f3907f38030bf11
1jinie <wkddnjswlsn@naver.com>
2026-02-09 18:25:21 +0900

    Add comment for type casting in division
    Added a comment explaining type casting in division.

--- 백준/Bronze/1008. A／B/A／B.java
+++ 백준/Bronze/1008. A／B/A／B.java
@@ -5,6 +5,7 @@ public class Main{
         Scanner sc = new Scanner(System.in);
         int a = sc.nextInt();
         int b = sc.nextInt();
-        System.out.println((double)a / (double)b);
+        System.out.println((double)a / (double)b);            // Type casting : Integer type a & b to double type
     }
+
 }
