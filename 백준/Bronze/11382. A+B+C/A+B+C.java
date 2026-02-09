1a81b493b59049bdb712a621fc27f914bc83af0c
1jinie <wkddnjswlsn@naver.com>
2026-02-10 00:25:20 +0900

    Add comments
    Added comments to clarify variable declaration and input reading.

--- 백준/Bronze/11382. A+B+C/A+B+C.java
+++ 백준/Bronze/11382. A+B+C/A+B+C.java
@@ -2,9 +2,10 @@ import java.util.Scanner;
 public class Main{
     public static void main(String[] args){
         Scanner sc = new Scanner(System.in);
-        long a = sc.nextLong();
+        long a = sc.nextLong();                                // declare long type a using .nextLong() : to read long type number
         long b = sc.nextLong();
         long c = sc.nextLong();
         System.out.println(a + b + c);
     }
 }
+
