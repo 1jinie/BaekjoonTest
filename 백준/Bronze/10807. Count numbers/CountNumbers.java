c44c16ef7bccf30c2507f750b7702e91c098c5c2
1jinie <wkddnjswlsn@naver.com>
2026-02-12 08:22:06 +0900

    Update CountNumbers.java
    Delete BufferedWriter

--- 백준/Bronze/10807. Count numbers/CountNumbers.java
+++ 백준/Bronze/10807. Count numbers/CountNumbers.java
@@ -4,7 +4,6 @@ import java.util.StringTokenizer;
 public class Main{
     public static void main(String[] args) throws IOException{
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
         int n = Integer.parseInt(br.readLine());                                                        // input numbers of integer
         int[] arr = new int[n];                                                                         // declare array with n
         StringTokenizer st = new StringTokenizer(br.readLine());
@@ -22,3 +21,4 @@ public class Main{
     }
 
 }
+
