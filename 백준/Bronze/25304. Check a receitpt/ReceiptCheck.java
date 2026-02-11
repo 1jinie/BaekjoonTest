2ed25d3cca61b3f3ed5e16e7d3e5f10fab5e9423
1jinie <wkddnjswlsn@naver.com>
2026-02-11 19:51:54 +0900

    Add comments
    Added comments to clarify code functionality.

--- 백준/Bronze/25304. Check a receitpt/ReceiptCheck.java
+++ 백준/Bronze/25304. Check a receitpt/ReceiptCheck.java
@@ -2,11 +2,11 @@ import java.util.Scanner;
 public class Main{
     public static void main(String[] args){
         Scanner sc = new Scanner(System.in);
-        int total = sc.nextInt();
-        int num = sc.nextInt();
+        int total = sc.nextInt();                            // total price
+        int num = sc.nextInt();                              // number of kinds of things
         int result = 0;
-        int[] arr = new int[num];
-        for(int i = 0; i < num; i++){
+        int[] arr = new int[num];                            // array of num
+        for(int i = 0; i < num; i++){                        // addition of each price and number to result
             arr[i] = sc.nextInt() * sc.nextInt();
             result += arr[i];
         }
@@ -16,4 +16,5 @@ public class Main{
             System.out.println("No");
         }
     }
+
 }
