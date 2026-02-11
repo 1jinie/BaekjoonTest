91d89dfb5946dd6d5c86938a13fd43a5112fbebd
1jinie <wkddnjswlsn@naver.com>
2026-02-11 19:34:08 +0900

    Add comments

--- 백준/Bronze/2739. Times table/TimesTable.java
+++ 백준/Bronze/2739. Times table/TimesTable.java
@@ -2,9 +2,10 @@ import java.util.Scanner;
 public class Main{
     public static void main(String[] args){
         Scanner sc = new Scanner(System.in);
-        int n = sc.nextInt();
-        for(int i = 1; i <= 9; i++){
+        int n = sc.nextInt();                                                    // input number
+        for(int i = 1; i <= 9; i++){                                             // n's times table
             System.out.printf("%d * %d = %d\n", n, i, n * i);
         }
     }
+
 }
