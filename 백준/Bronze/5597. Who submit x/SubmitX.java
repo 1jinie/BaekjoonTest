baa48577e6e93e62df31dadc4ac72384e51a94cd
1jinie <wkddnjswlsn@naver.com>
2026-02-12 16:12:01 +0900

    Create SubmitX.java

---
+++ 백준/Bronze/5597. Who submit x/SubmitX.java
@@ -0,0 +1,22 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main{
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int[] arr = new int[30];                                                                    // total students
+        for(int i = 0; i < 30; i++){                                                                // assign numbers
+            arr[i] = i + 1;
+        }
+        for(int j = 0; j < 30 -2; j++) {                                                            // if number is inputted, that element substitute with 0
+            int n = Integer.parseInt(br.readLine());
+            arr[n - 1] = 0;
+        }
+        for(int k = 0; k < 30; k++){                                                                // if element is not 0, print
+            if(arr[k] != 0){
+                System.out.println(arr[k]);
+            }
+        }
+    }
+}
