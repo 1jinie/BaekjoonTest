2d5be4b81d1cc19a4c919a31204c4f572107e5a2
1jinie <wkddnjswlsn@naver.com>
2026-02-12 09:29:13 +0900

    [Bronze III] Title: 최댓값, Time: 92 ms, Memory: 14144 KB -BaekjoonHub

---
+++ 백준/Bronze/2562. 최댓값/최댓값.java
@@ -0,0 +1,23 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main{
+    public static void main(String[] args) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int[] arr = new int[9];
+        for(int i = 0; i < arr.length; i++){
+            arr[i] = Integer.parseInt(br.readLine());
+        }
+        int max = arr[0];
+        int idx = 1;
+        for(int j = 0; j < arr.length; j++){
+            if(arr[j] > max){
+                max = arr[j];
+                idx = j + 1;
+            }
+        }
+        System.out.println(max);
+        System.out.println(idx);
+    }
+}
