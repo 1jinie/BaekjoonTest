0b18f1bffea2faed9393b78fa907076b4deb2558
1jinie <wkddnjswlsn@naver.com>
2026-02-12 17:21:11 +0900

    Add comments

--- 백준/Bronze/3052. Count remainders/CountRemain.java
+++ 백준/Bronze/3052. Count remainders/CountRemain.java
@@ -5,12 +5,12 @@ import java.io.InputStreamReader;
 public class Main{
     public static void main(String[] args) throws IOException{
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        boolean[] check = new boolean[42];
+        boolean[] check = new boolean[42];                                                            // numbers of remainders (0 ~ 41)
         int count = 0;
-        for(int i = 0; i < 10; i++){
+        for(int i = 0; i < 10; i++){                                                                  // 10 numbers
             int input = Integer.parseInt(br.readLine());
             int remain = input % 42;
-            if(!check[remain]){
+            if(!check[remain]){                                                                       // remainder count
                 check[remain] = true;
                 count++;
             }
