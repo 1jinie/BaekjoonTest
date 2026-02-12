cc4dcaff80a130e333e68aa572a3651d60739b70
1jinie <wkddnjswlsn@naver.com>
2026-02-12 13:16:42 +0900

    Add comments

--- 백준/Bronze/10813. Swap balls/SwapBalls.java
+++ 백준/Bronze/10813. Swap balls/SwapBalls.java
@@ -7,14 +7,14 @@ public class Main{
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st1 = new StringTokenizer(br.readLine());
-        int n = Integer.parseInt(st1.nextToken());
+        int n = Integer.parseInt(st1.nextToken());                                                // number of baskets
         int[] baskets = new int[n];
-        int m = Integer.parseInt(st1.nextToken());
-        for(int i = 0; i < n; i++){
+        int m = Integer.parseInt(st1.nextToken());                                                // how many times
+        for(int i = 0; i < n; i++){                                                               // put balls in each basket
             baskets[i] = i + 1;
         }
-        int temp = baskets[0];
-        for(int j = 0; j < m; j++){
+        int temp = baskets[0];                                                                    // initialize variable 'temp'
+        for(int j = 0; j < m; j++){                                                               // m times swap balls
             StringTokenizer st2 = new StringTokenizer(br.readLine());
             int a = Integer.parseInt(st2.nextToken());
             int b = Integer.parseInt(st2.nextToken());
@@ -22,8 +22,9 @@ public class Main{
             baskets[a - 1] = baskets[b - 1];
             baskets[b - 1] = temp;
         }
-        for(int k : baskets){
+        for(int k : baskets){                                                                     // show balls in baskets
             System.out.print(k + " ");
         }
     }
+
 }
