810826463c656c0df991f5a98b50b77abf7ead46
1jinie <wkddnjswlsn@naver.com>
2026-02-14 22:46:08 +0900

    [Bronze II] Title: 상수, Time: 88 ms, Memory: 14184 KB -BaekjoonHub

---
+++ 백준/Bronze/2908. 상수/상수.java
@@ -0,0 +1,18 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        String a = st.nextToken();
+        String b = st.nextToken();
+        String ra = new StringBuilder(a).reverse().toString();
+        String rb = new StringBuilder(b).reverse().toString();
+        int ia = Integer.parseInt(ra);
+        int ib = Integer.parseInt(rb);
+        System.out.println(Math.max(ia, ib));
+    }
+}
