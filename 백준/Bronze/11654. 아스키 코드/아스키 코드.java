42bcf5a8a0d4144622d01a55cede978872ef3856
1jinie <wkddnjswlsn@naver.com>
2026-02-14 18:33:29 +0900

    [Bronze V] Title: 아스키 코드, Time: 96 ms, Memory: 14140 KB -BaekjoonHub

---
+++ 백준/Bronze/11654. 아스키 코드/아스키 코드.java
@@ -0,0 +1,9 @@
+import java.io.*;
+
+public class Main{
+    public static void main(String[] args) throws IOException{
+        InputStreamReader isr = new InputStreamReader(System.in);
+        int a = isr.read();
+        System.out.println(a);
+    }
+}
