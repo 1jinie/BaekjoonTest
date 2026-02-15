895cc76c124394eaeb32234078e926e642ad90e5
1jinie <wkddnjswlsn@naver.com>
2026-02-15 10:57:46 +0900

    Add comments

--- 백준/Bronze/3003. Chess piece/ChessPiece.java
+++ 백준/Bronze/3003. Chess piece/ChessPiece.java
@@ -6,16 +6,17 @@ import java.util.StringTokenizer;
 public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int[] piece = {1, 1, 2, 2, 2, 8};
+        int[] piece = {1, 1, 2, 2, 2, 8};                                                        // original pieces
         int[] now = new int[6];
         StringTokenizer st = new StringTokenizer(br.readLine());
-        for(int i = 0; i < piece.length; i++){
+        for(int i = 0; i < piece.length; i++){                                                   // input pieces into now array
             now[i] = Integer.parseInt(st.nextToken());
         }
         StringBuilder sb = new StringBuilder();
-        for(int j = 0; j < piece.length; j++){
+        for(int j = 0; j < piece.length; j++){                                                   // how many pieces need
             sb.append(piece[j] - now[j]).append(" ");
         }
         System.out.print(sb);
     }
+
 }
