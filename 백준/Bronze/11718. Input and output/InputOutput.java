e9120f6ac94d0c1c9a08db4856408c0f460375f2
1jinie <wkddnjswlsn@naver.com>
2026-02-15 10:04:24 +0900

    Add a comment

--- 백준/Bronze/11718. Input and output/InputOutput.java
+++ 백준/Bronze/11718. Input and output/InputOutput.java
@@ -6,8 +6,9 @@ public class Main{
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         String line;
-        while((line = br.readLine()) != null){
+        while((line = br.readLine()) != null){                                                // Input and output with EOF
             System.out.println(line);
         }
     }
+
 }
