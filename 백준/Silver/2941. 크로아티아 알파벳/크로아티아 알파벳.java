5071a171227f4ae7ef4cf815c014affcedd5369f
1jinie <wkddnjswlsn@naver.com>
2026-02-16 17:57:37 +0900

    [Silver V] Title: 크로아티아 알파벳, Time: 108 ms, Memory: 14180 KB -BaekjoonHub

---
+++ 백준/Silver/2941. 크로아티아 알파벳/크로아티아 알파벳.java
@@ -0,0 +1,28 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String s = br.readLine();
+        if(s.contains("c=")){
+            s = s.replace("c=", "@");
+        } if(s.contains("c-")){
+            s = s.replace("c-", "@");
+        } if(s.contains("dz=")){
+            s = s.replace("dz=", "@");
+        } if(s.contains("d-")){
+            s = s.replace("d-", "@");
+        } if(s.contains("lj")){
+            s = s.replace("lj", "@");
+        } if(s.contains("nj")){
+            s = s.replace("nj", "@");
+        } if(s.contains("s=")){
+            s = s.replace("s=", "@");
+        } if(s.contains("z=")){
+            s = s.replace("z=", "@");
+        }
+        System.out.println(s.length());
+    }
+}
