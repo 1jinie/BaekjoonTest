9b866920948dbfe0285e1159f74793d0dd4a30a3
1jinie <wkddnjswlsn@naver.com>
2026-02-16 12:51:12 +0900

    Add comments

--- 백준/Bronze/10988. Check palindrome/Palindrome.java
+++ 백준/Bronze/10988. Check palindrome/Palindrome.java
@@ -5,10 +5,11 @@ public class Main{
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         boolean isPalindrome = false;
         String s = br.readLine();
-        String sr = new StringBuilder(s).reverse().toString();
-        if(s.equals(sr)){
+        String sr = new StringBuilder(s).reverse().toString();                                // reverse String
+        if(s.equals(sr)){                                                                     // compare two String
             isPalindrome = true;
         }
         System.out.println(isPalindrome ? 1 : 0);
     }
+
 }
