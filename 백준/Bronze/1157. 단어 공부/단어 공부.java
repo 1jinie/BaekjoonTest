e012f3d9eca96c2fc064b48319061554e28ade4f
1jinie <wkddnjswlsn@naver.com>
2026-02-16 14:06:34 +0900

    [Bronze I] Title: 단어 공부, Time: 256 ms, Memory: 21148 KB -BaekjoonHub

---
+++ 백준/Bronze/1157. 단어 공부/단어 공부.java
@@ -0,0 +1,29 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String word = br.readLine().toUpperCase();
+        int[] count = new int[26];
+        for(int i = 0; i < word.length(); i++){
+            count[word.charAt(i) - 'A']++;
+        }
+        int max = 0;
+        int modeCount = 0;
+        char mode = ' ';
+        for (int i : count) {
+            if (i > max) {
+                max = i;
+            }
+        }
+        for(int i = 0; i < count.length; i++){
+            if(max == count[i]){
+                modeCount++;
+                mode = (char)('A' + i);
+            }
+        }
+        System.out.println(modeCount > 1 ? "?" : mode);
+    }
+}
