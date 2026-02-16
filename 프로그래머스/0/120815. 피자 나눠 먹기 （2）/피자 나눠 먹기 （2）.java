6a95250d13b7e63bf0424c28f52f3bcdbe495d6a
1jinie <wkddnjswlsn@naver.com>
2026-02-16 18:59:12 +0900

    Add comments

--- 프로그래머스/0/120815. 피자 나눠 먹기 （2）/피자 나눠 먹기 （2）.java
+++ 프로그래머스/0/120815. 피자 나눠 먹기 （2）/피자 나눠 먹기 （2）.java
@@ -2,11 +2,11 @@ class Solution {
     public int solution(int n) {
         final int SLICE = 6;
         int gcd = gcd(n, SLICE);    
-        int pizza = (n * SLICE / gcd) / SLICE;
+        int pizza = (n * SLICE / gcd) / SLICE;              // 최대공약수로 최소공배수 구하기
         return pizza;
     }
     
-    public static int gcd(int a, int b){
+    public static int gcd(int a, int b){                    // 유클리드 호제법으로 최대공약수 구하기
         while(b != 0){
             int temp = a % b;
             a = b;
