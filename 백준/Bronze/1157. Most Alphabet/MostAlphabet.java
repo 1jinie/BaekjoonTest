b96023fa838a67c7f4f40a2cb259279b70adcfa3
1jinie <wkddnjswlsn@naver.com>
2026-02-16 14:25:49 +0900

    Add comments

--- 백준/Bronze/1157. Most Alphabet/MostAlphabet.java
+++ 백준/Bronze/1157. Most Alphabet/MostAlphabet.java
@@ -5,20 +5,20 @@ import java.io.InputStreamReader;
 public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        String word = br.readLine().toUpperCase();
-        int[] count = new int[26];
-        for(int i = 0; i < word.length(); i++){
+        String word = br.readLine().toUpperCase();                                            // String to uppercase
+        int[] count = new int[26];                                                            // to count alphabet number
+        for(int i = 0; i < word.length(); i++){                                               // count each alphabets by char
             count[word.charAt(i) - 'A']++;
         }
         int max = 0;
         int modeCount = 0;
         char mode = ' ';
-        for (int i : count) {
+        for (int i : count) {                                                                 // find maximum count
             if (i > max) {
                 max = i;
             }
         }
-        for(int i = 0; i < count.length; i++){
+        for(int i = 0; i < count.length; i++){                                                // output
             if(max == count[i]){
                 modeCount++;
                 mode = (char)('A' + i);
@@ -27,3 +27,4 @@ public class Main {
         System.out.println(modeCount > 1 ? "?" : mode);
     }
 }
+
