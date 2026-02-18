3037ca141c474cf5927fbbce7d9b04a89c32edd6
1jinie <wkddnjswlsn@naver.com>
2026-02-18 18:43:22 +0900

    [Silver V] Title: 너의 평점은, Time: 96 ms, Memory: 14604 KB -BaekjoonHub

---
+++ 백준/Silver/25206. 너의 평점은/너의 평점은.java
@@ -0,0 +1,42 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] arg) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String line, subject = "", gradeS = "";
+        double point = 0;
+        double grade = 0;
+        double sumGP = 0;
+        double sumP = 0;
+        double GPA = 0;
+        while((line = br.readLine()) != null){
+            StringTokenizer st = new StringTokenizer(line);
+            subject = st.nextToken();
+            point = Double.parseDouble(st.nextToken());
+            gradeS = st.nextToken();
+            switch(gradeS){
+                case "A+": grade = 4.5; break;
+                case "A0": grade = 4.0; break;
+                case "B+": grade = 3.5; break;
+                case "B0": grade = 3.0; break;
+                case "C+": grade = 2.5; break;
+                case "C0": grade = 2.0; break;
+                case "D+": grade = 1.5; break;
+                case "D0": grade = 1.0; break;
+                case "F": grade = 0; break;
+                default: break;
+            }
+            if(gradeS.equals("P")){
+                continue;
+            } else {
+                sumGP += grade * point;
+                sumP += point;
+                GPA = sumGP / sumP;
+            }
+        }
+        System.out.printf("%f",GPA);
+    }
+}
