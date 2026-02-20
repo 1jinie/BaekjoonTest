e1cf3c7b9db9b2843f34cc278d992d352fc8f74c
1jinie <wkddnjswlsn@naver.com>
2026-02-20 14:39:30 +0900

    [Bronze I] Title: 진법 변환 2, Time: 108 ms, Memory: 14368 KB -BaekjoonHub

---
+++ 백준/Bronze/11005. 진법 변환 2/진법 변환 2.java
@@ -0,0 +1,24 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        StringBuilder sb = new StringBuilder();
+        int n = Integer.parseInt(st.nextToken());
+        int b = Integer.parseInt(st.nextToken());
+        while(n > 0){
+             int r =  n % b;
+             if(r < 10) {
+                 sb.append((char)(r + '0'));
+             } else{
+                 sb.append((char)((r - 10) + 'A'));
+             }
+             n /= b;
+        }
+        System.out.println(sb.reverse());
+    }
+}
