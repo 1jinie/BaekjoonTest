cbe16cd72acefdbd1a6d25ea5ad45401f613ff74
1jinie <wkddnjswlsn@naver.com>
2026-02-20 15:46:40 +0900

    Add comments

--- 백준/Bronze/2720. Laundry charge/LaundryCharge.java
+++ 백준/Bronze/2720. Laundry charge/LaundryCharge.java
@@ -6,27 +6,27 @@ public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringBuilder sb = new StringBuilder();
-        int t = Integer.parseInt(br.readLine());
-        for(int i = 0; i < t; i++){
+        int t = Integer.parseInt(br.readLine());                                            // number of test case
+        for(int i = 0; i < t; i++){                                                         // repeat test case
             int c = Integer.parseInt(br.readLine());
-            int q = 0, d = 0, n = 0, p = 0;
+            int q = 0, d = 0, n = 0, p = 0;                                                 // quarter(25), dime(10), nickel(5), penny(1)
             while(c != 0){
-                while(c >= 25){
+                while(c >= 25){                                                             // quarter
                     q++;
                     c -= 25;
                 }
                 sb.append(q).append(" ");
-                while(c >= 10){
+                while(c >= 10){                                                             // dime
                     d++;
                     c -= 10;
                 }
                 sb.append(d).append(" ");
-                while(c >= 5){
+                while(c >= 5){                                                              // nickel
                     n++;
                     c -= 5;
                 }
                 sb.append(n).append(" ");
-                while(c >= 1){
+                while(c >= 1){                                                              // penny
                     p++;
                     c -= 1;
                 }
@@ -35,4 +35,5 @@ public class Main {
         }
         System.out.println(sb);
     }
+
 }
